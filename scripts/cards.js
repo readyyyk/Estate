08d40cc9a7d87c52749533f52b3ed0f5369f8d5c
readyyyk <mr.sairexer@gmail.com>
2022-10-21 22:57:26 +0300

    cards data rewrote on json

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,150 +1,6 @@
-const productsCardsData = {
-    type: "product",
-    innerData: [
-        {
-            type: "House",
-            title: "California St.",
-            beds: 16,
-            baths: 2,
-            rooms: 11,
-        },
-        {
-            type: "Villa",
-            title: "Welford Gardens",
-            beds: 12,
-            baths: 3,
-            rooms: 16,
-        },
-        {
-            type: "House",
-            title: "The Grove",
-            beds: 12,
-            baths: 2,
-            rooms: 14,
-        },
-        {
-            type: "Apartment",
-            title: "Wallace St.",
-            beds: 10,
-            baths: 2,
-            rooms: 8,
-        },
-        {
-            type: "Apartment",
-            title: "Dwight Way",
-            beds: 8,
-            baths: 1,
-            rooms: 6,
-        },
-        {
-            type: "House",
-            title: "North Avenue",
-            beds: 14,
-            baths: 3,
-            rooms: 11,
-        }],
-    parentEl: document.querySelector("#productsCards"),
-    imgsDir: "src/imgs/cards-1",
-    imgsPref: "cards-1",
-    imgsType: "jpg",
-    animationName: "animate__fadeInUp"
-}
-//renderCards(productsCardsData, productsCardsEl, productsImgsDir, productsImgsPref, productsImgsType, productsAnimation)
-
-const teamCardsData = {
-    type: "team",
-    innerData: [
-        {
-            name: "Ava Orn",
-            job: "Managing Broker"
-        },
-        {
-            name: "Ella Hessel",
-            job: "Agent"
-        },
-        {
-            name: "Daryl Gislason",
-            job: "REALTOR"
-        },
-        {
-            name: "David Sporer",
-            job: "Agent"
-        },
-        {
-            name: "Trevor Torphy",
-            job: "Agent"
-        },
-        {
-            name: "Amanda Giovanni",
-            job: "REALTOR"
-        }
-    ],
-    parentEl: document.querySelector("#teamCardsEl"),
-    imgsDir: "src/imgs/avatars",
-    imgsPref: "avatars",
-    imgsType: "png",
-    animationName: "animate__zoomIn"
-}
-//renderTeamCards(teamCardsData, teamCardsEl, teamImgsDir, teamImgsPref, teamImgsType)
-
-const newsroomCardsData = {
-    type: "news",
-    innerData: [
-        {
-            title: "Press release",
-            text: "Market watch: are condos still a good investment?",
-            date: "August 12, 2021"
-        },
-        {
-            title: "Announcement",
-            text: "The history of great design",
-            date: "August 12, 2021"
-        },
-        {
-            title: "Company news",
-            text: "Designers who changed everything",
-            date: "August 12, 2021"
-        }
-    ],
-    parentEl: document.querySelector("#newsroomCards"),
-    imgsDir: "src/imgs/cards-2",
-    imgsPref: "cards-2",
-    imgsType: "jpg",
-    animationName: "animate__fadeInUp"
-}
-// renderNewsroomCards(newsroomCardsData, newsroomCardsEl, newsroomCardsImgsDir, newsroomCardsImgsPref, newsroomCardsImgsType, newsroomCardsAnimation)
-
-const sliderData = {
-    type: "sliderEl",
-    innerData: [
-        {
-            stars: 5,
-            title: "We found the perfect home!",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
-            author: "Nate Davidsson"
-        },
-        {
-            stars: 4,
-            title: "We're so happy with Estate.",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.",
-            author: "Laura Paulie"
-        },
-        {
-            stars: 2,
-            title: "Thank you for our beautiful home.",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
-            author: "Greg Todd"
-        },
-        {
-            stars: 5,
-            title: "Smth for slider",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id. Star this project on <a href='https://github.com/readyyyk'> my github page </a>",
-            author: "Readyyyk"
-        }
-    ],
-    parentEl: document.querySelector("#slider-inner")
-}
-// renderSliderCards(sliderData, sliderEl)
+const dataCardsJSON = `[{"type":"product","innerData":[{"type":"House","title":"California St.","beds":16,"baths":2,"rooms":11},{"type":"Villa","title":"Welford Gardens","beds":12,"baths":3,"rooms":16},{"type":"House","title":"The Grove","beds":12,"baths":2,"rooms":14},{"type":"Apartment","title":"Wallace St.","beds":10,"baths":2,"rooms":8},{"type":"Apartment","title":"Dwight Way","beds":8,"baths":1,"rooms":6},{"type":"House","title":"North Avenue","beds":14,"baths":3,"rooms":11}],"imgsDir":"src/imgs/cards-1","imgsPref":"cards-1","imgsType":"jpg","animationName":"animate__fadeInUp","parentElId":"#productsCards"}, {"type":"team","innerData":[{"name":"Ava Orn","job":"Managing Broker"},{"name":"Ella Hessel","job":"Agent"},{"name":"Daryl Gislason","job":"REALTOR"},{"name":"David Sporer","job":"Agent"},{"name":"Trevor Torphy","job":"Agent"},{"name":"Amanda Giovanni","job":"REALTOR"}],"imgsDir":"src/imgs/avatars","imgsPref":"avatars","imgsType":"png","animationName":"animate__zoomIn","parentElId":"#teamCardsEl"}, {"type":"news","innerData":[{"title":"Press release","text":"Market watch: are condos still a good investment?","date":"August 12, 2021"},{"title":"Announcement","text":"The history of great design","date":"August 12, 2021"},{"title":"Company news","text":"Designers who changed everything","date":"August 12, 2021"}],"imgsDir":"src/imgs/cards-2","imgsPref":"cards-2","imgsType":"jpg","animationName":"animate__fadeInUp","parentElId":"#newsroomCards"}, {"type":"sliderEl","innerData":[{"stars":5,"title":"We found the perfect home!","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.","author":"Nate Davidsson"},{"stars":4,"title":"We're so happy with Estate.","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet.","author":"Laura Paulie"},{"stars":2,"title":"Thank you for our beautiful home.","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.","author":"Greg Todd"},{"stars":5,"title":"Smth for slider","text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id. Star this project on <a href='https://github.com/readyyyk'> my github page </a>","author":"Readyyyk"}],"parentElId":"#slider-inner"}]`
+    
+const dataCards = JSON.parse(dataCardsJSON)
 
 class ProductCard {
     constructor(el, imgsDir, imgPref, cnt, imgType, animationName) {
@@ -267,30 +123,33 @@ class SliderCard{
 
 function renderCards(cardsDataArray){
     cardsDataArray.forEach( (cardsData) => {
-        if(cardsData.type === "product"){
-            cardsData.innerData.forEach( (el, i) => {
-                let tempCard = new ProductCard(el, cardsData.imgsDir, cardsData.imgsPref, i, cardsData.imgsType, cardsData.animationName)
-                cardsData.parentEl.appendChild(tempCard.DOMelement)
-            } )
-        }
-        else if(cardsData.type === "team") {
-            cardsData.innerData.forEach( (el, i) => {
-                let tempCard = new TeamCard(el, cardsData.imgsDir, cardsData.imgsPref, i, cardsData.imgsType, cardsData.animationName)
-                cardsData.parentEl.appendChild(tempCard.DOMelement)
-            } )
-        }
-        else if(cardsData.type === "news") {
-            cardsData.innerData.forEach( (el, i) => {
-                let tempCard = new NewsroomCard(el, cardsData.imgsDir, cardsData.imgsPref, i, cardsData.imgsType, cardsData.animationName)
-                cardsData.parentEl.appendChild(tempCard.DOMelement)
-            } )
-        }
-        else if(cardsData.type === "sliderEl") {
-            cardsData.innerData.forEach( (el, i) => {
-                let tempCard = new SliderCard(el)
-                cardsData.parentEl.appendChild(tempCard.DOMelement)
-            } )
+        let parent = document.querySelector(cardsData.parentElId)
+        switch (cardsData.type){
+            case "product":
+                cardsData.innerData.forEach( (el, i) => {
+                    let tempCard = new ProductCard(el, cardsData.imgsDir, cardsData.imgsPref, i, cardsData.imgsType, cardsData.animationName)
+                    parent.appendChild(tempCard.DOMelement)
+                } )
+                break
+            case "team":
+                cardsData.innerData.forEach( (el, i) => {
+                    let tempCard = new TeamCard(el, cardsData.imgsDir, cardsData.imgsPref, i, cardsData.imgsType, cardsData.animationName)
+                    parent.appendChild(tempCard.DOMelement)
+                } )
+                break
+            case "news":
+                cardsData.innerData.forEach( (el, i) => {
+                    let tempCard = new NewsroomCard(el, cardsData.imgsDir, cardsData.imgsPref, i, cardsData.imgsType, cardsData.animationName)
+                    parent.appendChild(tempCard.DOMelement)
+                } )
+                break
+            case "sliderEl":
+                cardsData.innerData.forEach( (el, i) => {
+                    let tempCard = new SliderCard(el)
+                    parent.appendChild(tempCard.DOMelement)
+                } )
+                break
         }
     })
 }
-renderCards([productsCardsData, teamCardsData, newsroomCardsData, sliderData])
+renderCards(dataCards)
